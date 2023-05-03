a6c5f6046adce3f922ac35ec4c95b1beebb4c6f4
ricardo-paganini <ricardo.paganini@hotmail.com>
2023-05-03 10:16:03 -0300

    Criação do arquivo HelpFunctions, criação dos itens 3.1 à 3.6 do projeto

---
+++ HelpFunctions.py
@@ -0,0 +1,23 @@
+# =====================
+# Alterar tipo de dados
+# =====================
+
+def change_data_type(dataframe, column,data_type):
+    """
+    Objetivo: Alterar o tipo de dado de uma variável / feature do conjunto de dados
+    
+    Parâmetros solicitados: Dataframe, nome da coluna e tipo de dado
+    
+    Retorno: Dataframe com o tipo da variável / feature alterado
+    
+    Exemplo:
+        
+        change_data_type('Coluna1','int64')
+        change_data_type('Coluna2','float64')
+    """
+    
+    dataframe[column] = dataframe[column].astype(data_type)
+    
+    return dataframe
+    
+
